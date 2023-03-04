b3d5eaec459aefaf84f9c73986fbf66e8256d8cf
Chizzy Justin <nnabugwuchizitelu@gmail.com>
2023-03-04 16:21:06 +0100

    addition of env for port

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.get("/", function(req, res){
     res.render("index", {imageArray: imageArray});
 })
 
-
-app.listen(3000, function(){
+const port = process.env.PORT || 3000
+app.listen(port, function(){
     console.log("the portfolio is live on port 3000!")
 });
 
